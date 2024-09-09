3411463c159be6d10c05979e27e6cd0e3f701d8b
zahrabateninia <zahrabatenin@gmail.com>
2024-09-09 21:21:39 +0330

    Define initialValues of PersonalDetails component

--- CV-Application/src/components/PersonalDetails.jsx
+++ CV-Application/src/components/PersonalDetails.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import SectionForm from './SectionForm'
 
+
+ const initialValues = {
+  'full name': { value: '', type: 'text' },
+  'email': { value: '', type: 'email' },
+  'phone number': { value: '', type: 'tel' },
+  'city and province': { value: '', type: 'text' }
+};
+
+
 const PersonalDetails = () => {
   return (
-    <div>
-      
-    </div>
+    <SectionForm 
+        sectionTitle={"Personal Details"}
+        initialValues={initialValues}
+        
+    />
   )
 }
 
