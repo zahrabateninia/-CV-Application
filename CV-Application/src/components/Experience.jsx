8802dd3a1088aaeb6e99151105d6a151954afa07
zahrabateninia <zahrabatenin@gmail.com>
2024-09-18 16:25:38 +0330

    Import SelectionForm in Experience component and pass the props

--- CV-Application/src/components/Experience.jsx
+++ CV-Application/src/components/Experience.jsx
@@ -2,11 +2,29 @@ import React from 'react'
 import SectionForm from './SectionForm'
 
 
+const initialValues = {
+  'job title': '',
+  'compony': '',
+  'start date': '',
+  'end date': '',
+  'description':''
+};
+
+const inputTypes = {
+  'job title': 'text',
+  'compony': 'text',
+  'start date': 'date',
+  'end date': 'date',
+  'description':'text'
+}
+
 const Experience  = () => {
   return (
-    <div>
-      
-    </div>
+<SectionForm 
+        sectionTitle={"Professional Experience"}
+        initialValues={initialValues}
+        inputTypes={inputTypes}  
+    />
   )
 }
 
