e2f799cf796a80876190d86cb6fd8ec41a515580
zahrabateninia <zahrabatenin@gmail.com>
2024-09-18 15:44:41 +0330

    Import SelectionForm in Education component and pass the props

--- CV-Application/src/components/Education.jsx
+++ CV-Application/src/components/Education.jsx
@@ -2,11 +2,28 @@ import React from 'react'
 import SectionForm from './SectionForm'
 
 
+const initialValues = {
+  'degree': '',
+  'school': '',
+  'start date': '',
+  'end date': ''
+};
+
+const inputTypes = {
+  'degree': 'text',
+  'school': 'text',
+  'start date': 'date',
+  'end date': 'date'
+}
+
+
 const Education = () => {
   return (
-    <div>
-      
-    </div>
+    <SectionForm 
+        sectionTitle={"Educational Experience"}
+        initialValues={initialValues}
+        inputTypes={inputTypes}  
+    />
   )
 }
 
