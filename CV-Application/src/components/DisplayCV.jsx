0958e04062a431f51cc1fe9730b6cd5c665c6936
zahrabateninia <zahrabatenin@gmail.com>
2024-09-23 13:51:56 +0330

    Make a clear layout for displaying personal, educational, and professional information

--- CV-Application/src/components/DisplayCV.jsx
+++ CV-Application/src/components/DisplayCV.jsx
@@ -5,13 +5,37 @@ import '../styles/resume.css'
 const DisplayCV = () => {
   return (
     <div className='cv-container'>
-      {/* Personal Details */}
-      <section className='cv-personal-info'></section>
 
-      {/* Educational Details */}
-      <section className='cv-educational-info'></section>
-      {/* Professional Details */}
-      <section className='cv-professional-info'></section>
+      <section className='cv-personal-info'>
+        <h3 className='full-name'>full name</h3>
+        <div className='personal-details'>
+          <p className="email">email</p>
+          <p className="phone">my phone</p>
+          <p className="city">city</p>
+        </div>
+      </section>
+        
+      <section className='cv-educational-info'>
+      <h4>Educational Experience</h4>
+        <hr />
+        <div className='form-edu-info'>
+          <p className='degree'>degree</p>
+          <p className='school'>school</p>
+          <p className='date'>date</p>
+        </div>
+      </section>
+        
+      <section className='cv-professional-info'>
+      <h4>Professional Experience</h4>
+        <hr />
+        <div className='form-pro-info'>
+          <p className='job-title'>developer</p>
+          <p className="company">Google</p>
+          <p className="job-date">DATE</p>
+          <p className="job-description">hihgkhhh hdgh </p>
+        </div>
+      </section>
+        
     </div>
   )
 }
