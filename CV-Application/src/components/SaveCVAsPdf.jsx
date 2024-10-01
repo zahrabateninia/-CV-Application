466d61af1045661ae946f53cb52b1a960100cc66
zahrabateninia <zahrabatenin@gmail.com>
2024-10-01 16:16:53 +0330

    Generate pdf when download button's clicked. Use jsPDF & html2Canvas libraries.

--- CV-Application/src/components/SaveCVAsPdf.jsx
+++ CV-Application/src/components/SaveCVAsPdf.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
-import { FaDownload } from 'react-icons/fa'
+import React from 'react';
+import { FaDownload } from 'react-icons/fa';
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 const SaveCVAsPdf = () => {
-  return (
-    <button className="save-cv-btn">
-        <>
-        <FaDownload />
-        Download CV
-        </>
-    </button>
-  )
-}
+    const generatePdf = () => {
+        const cvElement = document.querySelector('.cv-container');
+        html2canvas(cvElement, { scale: 2 }).then(canvas => {
+            const imgData = canvas.toDataURL('image/png');
 
-export default SaveCVAsPdf
+            // Convert rem to points
+            const widthInPoints = 55 * 16 * 0.75; 
+            const heightInPoints = 57 * 16 * 0.75; 
+
+            const pdf = new jsPDF('portrait', 'pt', [widthInPoints, heightInPoints]);
+            
+            pdf.addImage(imgData, 'PNG', 0, 0, widthInPoints, heightInPoints);
+            pdf.save('resume.pdf');
+        });
+    };
+
+    return (
+        <button className="save-cv-btn" onClick={generatePdf}>
+            <>
+                <FaDownload />
+                Download CV
+            </>
+        </button>
+    );
+};
+
+export default SaveCVAsPdf;
